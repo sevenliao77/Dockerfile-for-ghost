fefb2e4b357ab98d32af32f3b4529ef3bcb10b17
hjl~(=^▽^=) <hujl_123@yeah.net>
2016-08-14 05:14:39 -0500

    add https

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 作者hjl
-// http://hjl.daoapp.io/
+// https://hjl.daoapp.io/
 // 请修改本文件
 
 
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://改成你要的前缀.daoapp.io/',
+        url: 'https://改成你要的前缀.daoapp.io/',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -31,7 +31,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'http://改成你要的前缀.daoapp.io/',
+        url: 'https://改成你要的前缀.daoapp.io/',
 
         // Example refferer policy
         // Visit https://www.w3.org/TR/referrer-policy/ for instructions
